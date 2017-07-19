e7ecc5e5d42911f776e0bb75068b68d5c32ee474
VaishakVk <vaishakhvkv@gmail.com>
2017-07-19 20:51:18 +0530

    lexicographically greater

---
+++ LexicographicalNext.py
@@ -0,0 +1,20 @@
+for _ in range(int(input())):
+    s = input()
+    grt = False
+    for i in range(len(s)-1)[::-1]:
+        if s[i] < s[i+1]:
+            grt = True
+            index = i
+            break
+    if grt == False:
+        print('no answer')
+    else:
+        for i in range(index+1,len(s))[::-1]:
+            if s[i] > s[index]:
+                lis = list(s)
+                lis[i],lis[index] = lis[index],lis[i]
+                print("".join(lis[:index+1] + lis[index+1:][::-1]))
+                break
+
+     
+   
